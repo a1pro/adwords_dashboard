1b896ee763e54c039749870422984d8f4267335c
a1pro <teamlead@a1professionals.com>
2016-01-18 18:21:22 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,44 +1,47 @@
- 	             function openTab(id){	 
-	                $('.main-tab').click(function(){
-					 var a =  'tab'+$(this).val();
-					  $('.abcdef').attr('id',a);
-					  $("input[name='name']").val(id);					  
-					  $.ajax({
-					  		url: "getcampaign.php?_cid="+$(this).val(),
-                            type: "POST",
-                            data : {},
-							success: function(data, textStatus, jqXHR)
-								{
-									//data - response from server
-									
-									$(".states-data").html(data);
-									$(".main_tab").hide();
-																	
-								},
-							error: function (jqXHR, textStatus, errorThrown)
-								{
-								}
+function openTab(id){	 
+$('.main-tab').click(function(){
+ var a =  'tab'+$(this).val();
+  $('.abcdef').attr('id',a);
+  $("input[name='name']").val(id);					  
+  $.ajax({
+  		url: "getcampaign.php?_cid="+$(this).val(),
+        type: "POST",
+        data : {},
+		success: function(data, textStatus, jqXHR)
+			{
+				//data - response from server
+				
+				$(".states-data").html(data);
+				$(".main_tab").hide();
+												
+			},
+		error: function (jqXHR, textStatus, errorThrown)
+			{
+			}
 
-					  });
-					});
-			  	}
-				$(function(){
+  		});
+	});
+}
+$(function(){
 
-				  	$('.get_type').click(function () {
-	                 	var type = $(this).text();
-	                 	//alert(type);
-	                 	$.ajax({ 
-	                 		url:"addcampaign.php?type="+type,
-	                 		type:"POST",
-	                 		data:{},
-	                 		success: function(data, textStatus, jqXHR){
-	                 				//alert(data);
-	                 				$(".addcamp").html(data);	                 				
-	                 		},
-	                 		error: function(jqXHR, textStatus, errorThrown){
-	                 		}
-	                 	});
+  	$('.get_type').click(function () {
+     	var type = $(this).text();
+     	//alert(type);
+     	$.ajax({ 
+     		url:"addcampaign.php?type="+type,
+     		type:"POST",
+     		data:{},
+     		success: function(data, textStatus, jqXHR){
+     				//alert(data);
+     				$(".addcamp").html(data);	                 				
+     		},
+     		error: function(jqXHR, textStatus, errorThrown){
+     		}
+     	});
+	});
+});
 
-	    			});
-    			});
+
+
+
     		
