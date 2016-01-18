4db00f00c60c7318f38e7b385c364c786a1aa843
a1pro <teamlead@a1professionals.com>
2016-01-18 18:20:42 +0530

    Update header.php

--- header.php
+++ header.php
@@ -23,11 +23,6 @@ $adwords_version ="v201509";
 		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
 		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>		
 		<script>
-		$(function() {
-		$( "#datepicker" ).datepicker();
-		});
-		$(function() {
-		$( "#datepicker1" ).datepicker();
-		});
+		
 		</script>	
 	</head>
