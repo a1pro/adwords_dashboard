8aeea44ff7cd6e553bdaabe88f16344b64da88c7
a1pro <teamlead@a1professionals.com>
2016-01-25 18:23:39 +0530

    Update header.php

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 include('function.php');
 require_once 'lib/Google/Api/Ads/AdWords/Lib/AdWordsUser.php';
 $adwords_version ="v201509"; 
+$user = new AdWordsUser();
 
 ?>
 <!DOCTYPE html>
@@ -12,17 +13,17 @@ $adwords_version ="v201509";
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
 		<title>Google Adwords</title>
-
 		<!-- Css Start -->
 		<link href="css/bootstrap.css" rel="stylesheet">
 		<link href="css/custom.css" rel="stylesheet">
-		<link href="fonts/calibri/stylesheet.css" rel="stylesheet">	
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>		
-		<script src="custom.js"></script>
-		 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		<link href="fonts/calibri/stylesheet.css" rel="stylesheet">		
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>		
-		<script>
-		
-		</script>	
+		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>	
+		<script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>		
+		<script src="custom.js"></script>
+	
 	</head>
+<body> 
